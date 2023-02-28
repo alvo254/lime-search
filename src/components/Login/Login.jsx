959bf15cdd533c3a216c724bbc510458e4a3adde
alvo254 <41018786+alvo254@users.noreply.github.com>
2023-02-28 15:37:24 +0300

    updates

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import axios from "../../api/axios"
+
 import "./Login.css"
 import { useRef, useState, useEffect } from 'react';
 
